a7d940495be08d835ef9c0b122320a2efa3b5163
PabloAceG <pabloacereda98@gmail.com>
2020-01-04 02:56:05 +0100

    Code maintenance.

--- README.md
+++ README.md
@@ -11,14 +11,24 @@ Proyecto de UBICUA: Sistema de control inteligente de plantas con Alexa
   *16 canales PWM
   *Puertos serie (com3)
  
-�� TENED ESTAS CARACTERÍSITICAS MUY EN CUENTA A LA HORA DE CREAR CÓDIGO QUE VAYA A CORRER SOBRE EL MICROCONTROLADOR!!
+�� TENED ESTAS CARACTERÍSITICAS MUY EN CUENTA A LA HORA DE CREAR CÓDIGO QUE 
+   VAYA A CORRER SOBRE EL MICROCONTROLADOR!!
 
-src/server --> servidor lanzado con JAVA Spring para tener un servicio WEB (estoy barajando el quitarlo por tiempo, y hacerlo directamente sobre HTML usando el IDE de ARDUINO, y que el servidor corra directamente sobre la IP de la microcontroladora como si fuera un LOCALHOST)
+src/server --> servidor lanzado con JAVA Spring para tener un servicio WEB 
+               (estoy barajando el quitarlo por tiempo, y hacerlo directamente 
+			   sobre HTML usando el IDE de ARDUINO, y que el servidor corra 
+			   directamente sobre la IP de la microcontroladora como si fuera 
+			   un LOCALHOST)
 
-src/ESPAlexa --> Dentro está la carpeta IRRIGATION_SYSTEM con el código que permite controlar el sistema de riego tanto por un comando de voz,
-como desde el panel de smartnest.cz. Además, con esto permitimos que el 
-usuario pueda crear rutinas (es decir, activar el riego cada 24 horas,
-por ejemplo). ECHARLE UN OJO, dentro del código tenéis los datos de 
-conexión a smartnest.cz para que veáis también cosas del proyecto.
+src/ESPAlexa --> Dentro está la carpeta IRRIGATION_SYSTEM con el código que 
+               permite controlar el sistema de riego tanto por un comando de 
+			   voz, como desde el panel de smartnest.cz. Además, con esto 
+		       permitimos que el usuario pueda crear rutinas (es decir, activar 
+			   el riego cada 24 horas, por ejemplo). ECHARLE UN OJO, dentro del 
+			   código tenéis los datos de conexión a smartnest.cz para que 
+			   veáis también cosas del proyecto.
 
-src\dsb18b20_to_WebServer --> Lectura del sensor DSB18b20 mostrando los datos en una web a la que nos conectamos usando la IP de red del ESP. Los datos en la web se muestran en Celsius y Farenhein y el diseño es muy básico, es una prueba de concepto más bien.
+src\dsb18b20_to_WebServer --> Lectura del sensor DSB18b20 mostrando los datos 
+               en una web a la que nos conectamos usando la IP de red del ESP. 
+			   Los datos en la web se muestran en Celsius y Farenhein y el 
+			   diseño es muy básico, es una prueba de concepto más bien.
--- src/ESP_ALEXA/IRRIGATING_SYSTEM/IRRIGATING_SYSTEM.ino
+++ src/ESP_ALEXA/IRRIGATING_SYSTEM/IRRIGATING_SYSTEM.ino
@@ -1,167 +1,233 @@
-/*--------------------------*/
-/*    L I B R A R I E S     */
-/*--------------------------*/
+/* ------------------------- */
+/*     L I B R A R I E S     */
+/* ------------------------- */
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include <WiFiClient.h>
 
-/*--------------------------*/
-/*    W I F I   D A T A     */
-/*--------------------------*/
-#define SSID_NAME "****"            // Changue to your Wifi Network name
-#define SSID_PASSWORD "******"     // Change to your Wifi network password
+/* ------------------------- */
+/*     W I F I   D A T A     */
+/* ------------------------- */
+#define SSID_NAME "NETWORK_NAME"      	// Wifi Network name
+#define SSID_PASSWORD "NETWOK_PASS"		// Wifi network password
 
-/*--------------------------*/
+/* ------------------------- */
 /*     S M A R T N E S T
  *     M Q T T  B R O K E R 
- *     D A T A 
- /*--------------------------*/
+ *     D A T A 				 */
+/* ------------------------- */
 #define MQTT_BROKER "smartnest.cz" //
 #define MQTT_PORT 1883 //
 #define MQTT_USERNAME "l.perezm" //
 #define MQTT_PASSWORD "Ubicua2019" //
-#define MQTT_CLIENT "5e0f5b6f4ff3c4137d21ef73" //Info provided by smartnest.cz/mynestV2 
+#define MQTT_CLIENT "5e0f5b6f4ff3c4137d21ef73" 	// Info provided by 
+											    // smartnest.cz/mynestV2 
 
 
-/*--------------------------*/
-/*    V A R I A B L E S     */
-/*--------------------------*/
-
-WiFiClient espClient;
+/* ------------------------- */
+/*     V A R I A B L E S     */
+/* ------------------------- */
+WiFiClient   espClient;
 PubSubClient client(espClient); 
-int waterPump=2;
+int          waterPump = 2;
 
 
-/*--------------------------*/
-/*     F U N C T I O N 
- *     D E C L A R A T I O N
-/*--------------------------*/
+/*------------------------- */
+/*   F U N C T I O N 
+ *   D E C L A R A T I O N  */
+/*------------------------- */
 void WiFiConnection();
-void initMQTT();
-void checkMqtt();
-int splitTopic(char* topic, char* tokens[] ,int tokensNumber);
-void callback(char* topic, byte* payload, unsigned int length);
+void initMQTT      ();
+void checkMqtt     ();
+int  splitTopic    (char* topic, char* tokens[] ,int tokensNumber);
+void callback      (char* topic, byte* payload,  unsigned int length);
 
 
 
-/*--------------------------*/
-void setup() {
+/* ------------------------ */
+void setup() 
+{
+  // Initializes the water pump.
   pinMode(waterPump, OUTPUT);
   Serial.begin(115200);
+  // Connects to the WiFi Network.
   WiFiConnection();
+  // Starts the MQTT Service.
   initMQTT();
 }
 
-void loop() {
+void loop() 
+{
+  // Must be kept alive to process incoming messages and maintain connection to
+  // the server.
   client.loop();
+  // In case MQTT is closed, it starts the service.
   checkMqtt();
 }
-/*--------------------------*/
 
-// Function to execute when a new msg has been received
+/* ------------------------ */
+
 /*
+ * Executes when a new message is received.
+ *
  * CALLBACK FUNCTION IMPORTANT INFO:
  * MQTT HAS 3 BASIC CONCEPTS:
- *    1. TOPICS: is like the address for a msg.
- *         In our case, it woul look like Home/bedroom/waterpumpAlexa/Turn
- *         Where the msg is ON or OFF
- *    2. PUBLISH/SUBSCRIBE SYSTEM: Every device can subscribe to topics, and
- *       also can receive all the msg sent with the topics they are subscibe.
- *       ! WHEN THE DEVICE WANT TO PUBLISH A MSG THE TOPIC MUST BE PROVIDED
- *    3. BROKER: Is the central device who allows and restricts connections 
- *       and also, receives, filters, redirects and publishes msg to 
- *       all devices connected. In this case, the broker is Smartnest Server
- *
+ *    1. TOPICS: 
+ *       Similar to the address of a message.
+ *       In this case, it should look like Home/bedroom/waterpumpAlexa/Turn.
+ *       Where the msg is ON or OFF.
+ *    2. PUBLISH/SUBSCRIBE SYSTEM: 
+ *       Devices can subscribe to any topic and, of course, receove the 
+ *       messages from those topics they are subscribed to.
+ *       In order to publish a message, a topic must be provided!
+ *    3. BROKER: 
+ *       Central device. It allows and restricts the connections. Its job is 
+ *       also to receive, filter, redirect and publish messages to all the 
+ *       devices connected. 
+ *       In this case, the broker is Smartnest Server.
  */
-void callback(char* topic, byte* payload, unsigned int length) { 
-  Serial.print("Topic:");
+void callback(char* topic, byte* payload, unsigned int length) 
+{ 
+  // Representation of the connected topic.
+  Serial.print  ("Topic:");
   Serial.println(topic);
-   int tokensNumber=10;
-   char *tokens[tokensNumber];
-   char message[length+1];
-   splitTopic(topic, tokens, tokensNumber);
-   sprintf(message,"%c",(char)payload[0]);
-   for (int i = 1; i < length; i++) {
-    sprintf(message,"%s%c",message,(char)payload[i]);
-   }
-    Serial.print("Message:");
-    Serial.println(message);
+  
+  // Obtains the topic and prints the message.
+  int tokensNumber = 10;
+  char *tokens [tokensNumber];
+  char  message[length + 1];
+  splitTopic(topic, tokens, tokensNumber);
+  
+  sprintf(message, "%c", (char) payload[0]);
+  for (int i = 1; i < length; i++) 
+  {
+    sprintf(message, "%s%c", message, (char) payload[i]);
+  }
+  
+  // Prints message.
+  Serial.print  ("Message:");
+  Serial.println(message);
     
-     char reportChange[100];
-     sprintf(reportChange,"%s/report/powerState",MQTT_CLIENT);
- 
-//------------------ACTIONS HERE---------------------------------
+  // Notifies the experienced state alteration due to the received message.
+  char reportChange[100];
+  sprintf(reportChange, "%s/report/powerState", MQTT_CLIENT);
 
-  if(strcmp(tokens[1],"directive")==0 && strcmp(tokens[2],"powerState")==0){
-    if(strcmp(message,"ON")==0){
+  // Sends the new status to the Server.
+  // If this were to be placed in the loop, the device would get locked. 
+  if(strcmp(tokens[1], "directive")  == 0 && 
+     strcmp(tokens[2], "powerState") == 0)
+  {
+	// Starts the water pump.
+    if(strcmp(message, "ON") == 0)
+	{
       digitalWrite(waterPump, LOW); 
-      client.publish(reportChange, "ON") ;            // Sends the new status to the Server, Do not put this in a for loop or your device will be blocked 
-      }
-    else if(strcmp(message,"OFF")==0){
+      client.publish(reportChange, "ON") ;            
+    }
+	// Stops the water pump.
+    else if(strcmp(message, "OFF") == 0)
+	{
       digitalWrite(waterPump, HIGH);                   
-      client.publish(reportChange, "OFF");           // Sends the new status to the Server, Do not put this in a for loop or your device will be blocked 
-      }
+      client.publish(reportChange, "OFF");           
+	}
   }
 }
 
-// Connect ESP to WIFI
-void WiFiConnection(){
-  if(WiFi.status() != WL_CONNECTED){
-     WiFi.mode(WIFI_STA);
-     WiFi.begin(SSID_NAME, SSID_PASSWORD);
-     Serial.println("Connecting ...");
-     while (WiFi.status() != WL_CONNECTED) {
-       delay(500);
-       Serial.print(".");
-     }
+/*
+ * Connection to the WiFi Network.
+ */
+void WiFiConnection()
+{
+  // Connect in case it has not been connected already.
+  if(WiFi.status() != WL_CONNECTED)
+  {
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(SSID_NAME, SSID_PASSWORD);
+	// Tries to connect
+    Serial.println("Connecting ...");
+    while (WiFi.status() != WL_CONNECTED) 
+	{
+      delay(500);
+      Serial.print(".");
+    }
   } 
+  
+  // Connected Successfully. 
   Serial.println('\n');
-  Serial.print("Connected to ");
+  Serial.print  ("Connected to ");
   Serial.println(WiFi.SSID());             
-  Serial.print("IP address:\t");
+  Serial.print  ("IP address:\t");
   Serial.println(WiFi.localIP());
   delay(500);  
-  }
+}
 
-// Starting MQTT service
-  void initMQTT(){
-  client.setServer(MQTT_BROKER, MQTT_PORT);
+/*
+ * Initialization of MQTT service.
+ */
+void initMQTT()
+{
+  client.setServer  (MQTT_BROKER, MQTT_PORT);
   client.setCallback(callback);
 
-  while (!client.connected()) {
+  while (!client.connected()) 
+  {
     Serial.println("Connecting to MQTT...");
-    if (client.connect(MQTT_CLIENT, MQTT_USERNAME, MQTT_PASSWORD )) {
-      Serial.println("connected");
-      } else {
-      Serial.print("failed with state ");
+	// Conected
+    if (client.connect(MQTT_CLIENT, MQTT_USERNAME, MQTT_PASSWORD )) 
+	{
+      Serial.println("Connected");
+    } 
+	// Failed Connection
+	else 
+	{
+      Serial.print("Failed with state ");
       Serial.print(client.state());
       delay(0x7530);
     }
   }
   
-  char reportTopic[100];
+  // Initialization of the Topic and Subscriber.
+  // Allows to show in the Serial any request to the service.
+  char reportTopic [100];
   char publishTopic[100];
-  sprintf(reportTopic,"%s/report/online",MQTT_CLIENT);
-  sprintf(publishTopic,"%s/#",MQTT_CLIENT);
+  
+  sprintf(reportTopic,  "%s/report/online", MQTT_CLIENT);
+  sprintf(publishTopic, "%s/#",             MQTT_CLIENT);
   client.subscribe(publishTopic);
-  client.publish(reportTopic, "true");
-  }
+  client.publish  (reportTopic, "true");
+}
 
-// SPLIT THE TOPIC. Eg: Home/bedroom/waterpumpAlexa/Turn --> [Home, bedroom, waterpumpAlexa, Turn]
-  int splitTopic(char* topic, char* tokens[],int tokensNumber ){
-    const char s[2] = "/";
-    int pos=0;
-    tokens[0] = strtok(topic, s);
-    while(pos<tokensNumber-1 && tokens[pos] != NULL ) {
-        pos++;
-      tokens[pos] = strtok(NULL, s);
-    }
-    return pos;
+/*
+ * Splits a string by the character "/". 
+ * Can be specifically used to split topics.
+ * Eg.: 
+ * input:  Home/bedroom/waterpumpAlexa/Turn
+ * output: [Home, bedroom, waterpumpAlexa, Turn]
+ */
+int splitTopic(char* topic, char* tokens[], int tokensNumber)
+{
+  const char s[2] = "/";
+  int pos         = 0;
+  
+  // Breaks the input into tokens 
+  tokens[0] = strtok(topic, s);
+  
+  while(pos         <  tokensNumber - 1 && 
+        tokens[pos] != NULL) 
+  {
+    pos++;
+    tokens[pos] = strtok(NULL, s); 
   }
+  
+  return pos;
+}
 
-// IF MQTT HASN'T BEEN INIT BEFORE, TRY IT AGAIN
-  void checkMqtt(){
-    if(!client.connected()){
-    initMQTT();
+/* 
+ * Starts the MQTT service if it has not been initialized yet.
+ */
+void checkMqtt ()
+  {
+    if(!client.connected())
+	{
+      initMQTT();
     }
-  }
+}
