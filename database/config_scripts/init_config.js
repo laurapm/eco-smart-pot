5196983eced92f9153430cc96011ddef8249184f
Pablo Acereda <pabloacereda98@gmail.com>
2020-08-10 12:28:37 +0200

    Initial data insertion

--- database/config_scripts/init_config.js
+++ database/config_scripts/init_config.js
@@ -39,7 +39,8 @@ db = db.getSiblingDB('eco')
 db.model_version.insert(
    [
      { version: "0", changes: "Creation of eco database and model_version and a way to track changes between versions." },
-     { version: "0.1", date: new Date(), changes: "Created users." }
+     { version: "0.1", date: new Date(), changes: "Created users." },
+     { version: "0.2", date: new Date(), changes: "Schema design: Add 6 collections (owner, device, reminder, measurements, plant, treatment)"},
+     { version: "0.2.1", date: new Date(), changes: "Apply MongoDB patterns to collections."}
    ]
 )
-
