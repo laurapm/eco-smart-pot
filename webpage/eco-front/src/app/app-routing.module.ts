fc410085824635e1784ddb02119e0a332aa70c9f
Pablo Acereda <pabloacereda98@gmail.com>
2020-08-13 16:35:33 +0200

    - Include logos - Navgar reorganization (left: menu; middle: company name; right: cart   and login) - Routing for login page (design card not done in this version) - Connection class for MongoDB

--- webpage/eco-front/src/app/app-routing.module.ts
+++ webpage/eco-front/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import {ShoppingCardComponent} from './componentes/shopping-card/shopping-card.component'
+import { NgModule }              from '@angular/core';
+import { Routes, RouterModule }  from '@angular/router';
+import { ShoppingCardComponent } from './componentes/shopping-card/shopping-card.component'
+import { LoginCardComponent }    from './componentes/login-card/login-card.component'
 
 const routes: Routes = [
-  {path: 'product', component: ShoppingCardComponent}
+  { path: 'product', component: ShoppingCardComponent },
+  { path: 'login',   component: LoginCardComponent }
 ];
 
 @NgModule({
---
+++ webpage/eco-webpage/src/config/MongoConfig.java
@@ -0,0 +1,29 @@
+package org.rainforest.ecowebpage;
+
+import com.mongodb.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+
+@Configuration
+public class MainConfig 
+{
+	@Autowired
+    private Environment env;
+
+    @Bean
+    public MongoDatabaseFactory mongoDbFactory() {
+        return new SimpleMongoDbFactory(new MongoClientURI(env.getProperty("spring.data.mongodb.uri")));
+    }
+
+	@Bean
+	public MongoTemplate mongoTemplate() {
+		MongoTemplate mongoTemplate = new MongoTemplate(mongoDbFactory());
+
+		return mongoTemplate;
+    }
+}
