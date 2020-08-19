630900840f95919bca1af0f2b8eebec88844ca39
Pablo Acereda <pabloacereda98@gmail.com>
2020-08-19 12:27:38 +0200

    Frontend REST API for login (need to fix some bugs)

--- webpage/eco-front/src/app/components/authentication-card/login-card/login-card.component.ts
+++ webpage/eco-front/src/app/components/authentication-card/login-card/login-card.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService }         from '../../../services/auth.service'
+import { TokenStorageService } from '../../../services/token-storage.service'
+
 @Component({
   selector: 'login-card',
   templateUrl: './login-card.component.html',
@@ -7,9 +10,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginCardComponent implements OnInit {
 
-  user: {};
+  form: any = { };
+  isLoggedIn    = false;
+  isLoginFailed = false;
+  errorMessage = '';
+  roles: string[] = [];
 
-  constructor( ) { }
+  constructor(
+    private authService: AuthService,
+    private tokenStorage: TokenStorageService) { }
 
-  ngOnInit(): void {  }
+  ngOnInit(): void {
+    if (this.tokenStorage.getToken()) {
+      this.isLoggedIn = true;
+      this.roles = this.tokenStorage.getUser().roles;
+    }
+  }
+
+  onSubmit():void {
+    this.authService.login(this.form).subscribe(
+      data => {
+        this.tokenStorage.saveToken(data.accessToken);
+        this.tokenStorage.saveUser(data);
+
+        this.isLoginFailed = false;
+        this.isLoggedIn    = true;
+        this.roles = this.tokenStorage.getUser().roles;
+        this.reloadPage();
+      },
+      err => {
+        this.errorMessage = err.error.message;
+        this.isLoginFailed = true;
+      }
+    );
+  }
+
+  reloadPage(): void {
+    window.location.reload();
+  }
+
 }
